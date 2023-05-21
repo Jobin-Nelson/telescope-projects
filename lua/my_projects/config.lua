5917138f6fecdfe729d12355d5e0c89452c9c255
Jobin Nelson <jobinnelson369@gmail.com>
2023-05-21 14:48:36 +0530

    add get_project_dir function instead of variable

--- lua/my_projects/config.lua
+++ lua/my_projects/config.lua
@@ -1,9 +1,13 @@
 local config = {}
 
-config.project_dir = ''
+local project_dir = ''
 
 config.setup = function(opts)
-  config.project_dir = opts.project_dir or '~/playground/projects'
+  project_dir = opts.project_dir or '~/playground/projects'
+end
+
+config.get_project_dir = function()
+  return project_dir
 end
 
 return config
