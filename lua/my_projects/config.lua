fca3132bcdae938a13b5a8681cda3ccb32adca63
Jobin Nelson <jobinnelson369@gmail.com>
2023-05-21 14:14:59 +0530

    change M.setup to config.setup

--- lua/my_projects/config.lua
+++ lua/my_projects/config.lua
@@ -1,13 +1,13 @@
-local M = {}
+local config = {}
 
-M.project_dir = ''
+config.project_dir = ''
 
-M.setup = function(opts)
+config.setup = function(opts)
   if opts['project_dir'] == nil then
     error('project_dir is required')
   end
 
-  M.project_dir = opts.project_dir
+  config.project_dir = opts.project_dir
 end
 
-return M
+return config
